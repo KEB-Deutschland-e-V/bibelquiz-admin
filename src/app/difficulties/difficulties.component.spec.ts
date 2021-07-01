5ff43c496b74f62adf222f87a1f4932a88a05f14
Dominik Sigmund <dominik.sigmund@br.de>
2021-07-01 09:07:24 +0200

    first commit

---
+++ src/app/difficulties/difficulties.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DifficultiesComponent } from './difficulties.component';
+
+describe('DifficultiesComponent', () => {
+  let component: DifficultiesComponent;
+  let fixture: ComponentFixture<DifficultiesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DifficultiesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DifficultiesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
