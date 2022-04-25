b3bf1e3a390aed39ff7397eec1954bd79427b6b4
Dominik Sigmund <dominik.sigmund@br.de>
2022-04-25 09:14:00 +0200

    fix

--- motor.sh
+++ motor.sh
@@ -7,8 +7,8 @@ docker rm bibelquiz-admin || :
 docker run \
 --name bibelquiz-admin \
 --volume /opt/bibelquiz/admin/app:/app \
---env SECRET_KEY_BASE=motor \
---env DATABASE_URL: postgresql://motor:motor@localhost:5432/motor \
+--env SECRET_KEY_BASE=e59d57183e994efcf9bfcf3f7f326f48d0248706db9551694d03ec034a209850dfd9604c27dacea9a52ea74ccf959e94dc8ce8748af56767661e7ded28d56b4c \
+--env DATABASE_URL=postgresql://motor:motor@localhost:5432/motor \
 --restart unless-stopped \
 --network="host" \
 -d motoradmin/motoradmin:latest
